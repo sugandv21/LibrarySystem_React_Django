a3dfe6bc0ed120f04ae4dfc6beb57f2cc95482d9
sugandv21 <sdvsuganya@gmail.com>
2025-09-09 21:37:38 +0530

    Update settings.py

--- backend/library_project/settings.py
+++ backend/library_project/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://sslibrarysystem.netlify.app",
 ]
 
 REST_FRAMEWORK = {
@@ -136,3 +137,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
