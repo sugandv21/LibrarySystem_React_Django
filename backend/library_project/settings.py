0e4859912c18f0b922a24af80e69ea252b49ba15
sugandv21 <sdvsuganya@gmail.com>
2025-09-09 21:43:08 +0530

    Update settings.py

--- backend/library_project/settings.py
+++ backend/library_project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret.!
 SECRET_KEY = 'django-insecure-85&olc=ky7_ra*k4m4k2xoxe3fvwuq8o2m)c06e%%w&l%3r@9&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -137,4 +137,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
